0eeee75d4062632fef6a6d1010a77c88d2fcc24c
HukovaNastya <hukovanasta@gmail.com>
2025-11-09 20:02:59 +0200

    added-search-form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,22 @@
 const SearchForm = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Hello word!')
+    }
+
     return (
-        <h1>Hello WOrd!</h1>
+        <form onSubmit={handleSubmit} className="search-section">
+                <div className="search-box">
+                    <input
+                        autoComplete="off"
+                        className="search-input"
+                        id="searchInput"
+                        placeholder="Enter GitHub username..."
+                        type="text"
+                    />
+                    <button className="btn btn-primary" id="searchBtn">Search</button>
+                </div>
+        </form>
     )
 }
 
